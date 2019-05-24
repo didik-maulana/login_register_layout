0527d7f85377a5a0557cc27be8723814a7fb5031
Didik Maulana Ardiansyah <didikmaulana49@gmail.com>
2019-05-24 10:57:50 +0700

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:login_register_layout/screens/register_view.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: "Login Register Layout",
+    title: "Login Register Page",
     initialRoute: "/",
     routes: {
       "/" : (context) => LoginPage(),
